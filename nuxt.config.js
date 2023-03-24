1cecea22b50f1e9e086b9a0dc47b59edd3a2e40f
hodgesiv <40799116+hodgesiv@users.noreply.github.com>
2023-03-23 20:24:54 -0400

    update storyblok preview token

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
     [
       'storyblok-nuxt',
       {
-        accessToken: 'csVfYUOdFMmdfNirvMnGBgtt',
+        accessToken: 'Do54rHsEnQocxmp7GJS2ygtt',
         cacheProvider: 'memory'
       }
     ],
